c4d673e78643d2faec909557633ecaedd09de6a6
GeorgiAghajanyan3 <georgi.hagajanyan@gmail.com>
2026-07-16 00:24:48 +0400

    Fix Add CI pipeline

--- frontend/src/components/RecentProjects.jsx
+++ frontend/src/components/RecentProjects.jsx
@@ -12,7 +12,7 @@ export default function RecentProjects() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/projects/")
+    fetch(`${import.meta.env.VITE_API_URL}/projects/`)
       .then((res) => res.json())
       .then((data) => {
         setProjects(data.slice(-5).reverse());
